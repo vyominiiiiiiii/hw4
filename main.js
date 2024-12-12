5b6d90ff44b6452da93bffc4083025b15d483fd1
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2024-12-12 11:12:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var press=()=>{
 var x=document.getElementById("enter").value;
-var time=new Date().getHours(x);
+var time=new Date().getHours();
 console.log(time);
-if(time<10){
-    console.log("Good Morning Harry");
-    document.getElementById("show").innerHTML="Good morning Harry";
+if(time<12){
+    console.log("Good Morning "+x);
+    document.getElementById("show").innerHTML="Good morning "+x;
 }
 else if(time<14){
-    console.log("Good Afternoon Harry");
-    document.getElementById("show").innerHTML="Good afternoon Harry";}
+    console.log("Good Afternoon "+x);
+    document.getElementById("show").innerHTML="Good afternoon "+x;}
 else{
-    console.log("Good Evening Harry");
-    document.getElementById("show").innerHTML="Good evening Harry";
+    console.log("Good Evening "+x);
+    document.getElementById("show").innerHTML="Good evening "+x;
 }
 }
